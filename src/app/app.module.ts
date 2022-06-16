0d4e8b0a66f506d24d5746868e8cf4508f6c5adc
juanbep <jbecapillimue@gmail.com>
2022-06-15 23:09:19 -0500

    vista reservar espacios 1.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,16 @@ import { CanchasComponent } from './components/reservar-espacios/reservar_espaci
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatListModule} from '@angular/material/list';
+import { MatButtonModule} from '@angular/material/button';
 
+
+//import { AppRoutingModule } from './app-routing.module';
 
 
 const appRoutes:Routes=[
@@ -36,6 +45,13 @@ const appRoutes:Routes=[
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
     FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+
+  //fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
+  fillerNav=[
+    {name:"Reservar Espacios",route:"",icon:"home"},
+    {name:"Historial",route:"contacto",icon:"perm_contact_calendar"},
+    {name:"Horarios Fijos",route:"card",icon:"card_giftcard"},
+    {name:"Gestionar Escenarios",route:"tabs",icon:"card_giftcard"}
+  
+  ]
+
+
+  fillerContent = Array.from({length: 50}, () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = true;
 
   ngOnInit(): void {
   }
