21ccbee22726e88c62d29dc46d44ea420b7efd2f
santiago9739 <65146938+santiago9739@users.noreply.github.com>
2022-07-01 11:27:23 -0500

    component calendario

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -2,16 +2,11 @@ import { Component, NgModule, OnInit } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
-
-
-
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.css'],
 })
-
-
 
 export class CalendarioComponent implements OnInit {
 
