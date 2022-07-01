ff4de1064e0599dc97815270612b4aa800e8deb3
santiago9739 <65146938+santiago9739@users.noreply.github.com>
2022-07-01 10:55:43 -0500

    Actualizacion Componente Calendario

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
-  styleUrls: ['./calendario.component.css']
+  styleUrls: ['./calendario.component.css'],
 })
+
+
+
 export class CalendarioComponent implements OnInit {
 
   constructor() { }
-
+  listHoras:any[]=[];
   ngOnInit(): void {
+    for (let index = 0; index < 17; index++) {
+      this.listHoras[index]=index+6;
+    }
+
   }
+  foods: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
 
 }
