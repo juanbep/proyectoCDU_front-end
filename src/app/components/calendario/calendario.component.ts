fabb7d2da39267cacf04c57ba8eb026a100f536d
juanbep <jbecapillimue@gmail.com>
2022-07-26 08:27:18 -0500

    visualizar horarios ya casi 2.0

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,8 +1,14 @@
-import { Component, NgModule, OnInit, Input } from '@angular/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { Component, NgModule, OnInit, Input, ViewChild, ElementRef, Output, EventEmitter, Renderer2,
+AfterViewInit} from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Horario } from 'src/app/interfaces/horario';
 import { HorarioService } from 'src/app/services/horario.service';
+import { Data } from '@angular/router';
+import { SelectorContext } from '@angular/compiler';
+import { style } from '@angular/animations';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+
 
 declare function greet(): void;
 
@@ -11,35 +17,47 @@ declare function greet(): void;
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.css'],
 })
-
-export class CalendarioComponent implements OnInit {
-
-  horarios: Horario[]=[];
+export class CalendarioComponent  implements AfterViewInit{
+  
+  horarios: Horario[] = [];
   myScriptElement: HTMLScriptElement;
 
-  celda: string = "lunes8";
+  @ViewChild('lunes18', {static : false}) title : any ;
+     
+  public texto: string = "";
 
-  constructor(private horarioservice:HorarioService) {
-    this.myScriptElement = document.createElement("script");
-    this.myScriptElement.src = "src/assets/js/horario.js";
+  constructor(private horarioservice: HorarioService, private renderer2: Renderer2) {
+    this.myScriptElement = document.createElement('script');
+    this.myScriptElement.src = 'src/assets/js/horario.js';
     document.body.appendChild(this.myScriptElement);
   }
 
-  listHoras:any[]=[];
+  ngAfterViewInit(): void {
+    const asTitle = this.title.nativeElement;
+    this.renderer2.setStyle(asTitle, 'background-color', 'red');
+  }
+
+  listHoras: any[] = [];
   ngOnInit(): void {
     for (let index = 0; index < 17; index++) {
-      this.listHoras[index]=index+6;
+      this.listHoras[index] = index + 6;
     }
-    this.horarioservice.getHorariosInfo().subscribe(
-      e => {this.horarios=e; console.log(this.horarios)}
-    );
+    this.horarioservice.getHorariosInfo().subscribe((e) => {
+      this.horarios = e;
+      console.log(this.horarios);
+    });
+    this.texto = "hola mundo";
+    
+  }
+
+  change(): void{
+    const asTitle = this.title.nativeElement;
+    this.renderer2.setStyle(asTitle, 'background-color', 'red');
   }
 
   foods: any[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
   ];
-
-
 }
--- src/assets/js/horario.js
+++ src/assets/js/horario.js
@@ -1,13 +1,15 @@
-function cambia_color(){
-    celda = document.getElementById("celda_" + document.fcolor.celda.value)
-    celda.style.backgroundColor=document.fcolor.micolor.value
+function cambia_color() {
+  celda = document.getElementById("celda" + document.fcolor.celda.value);
+  celda.style.backgroundColor = document.fcolor.micolor.value;
 }
 
-function cambiar_color_over(celda, color){
-    celda.style.backgroundColor= color;
- }
- 
-function cambiarColor(celda) {
-    const element = document.getElementById(celda);
-    element.style.backgroundColor = "#66ff33";
+function cambiar_color_over(celda, color) {
+  celda.style.backgroundColor = color;
+}
+
+function cambiar_color_over(celda) {
+  celda.style.backgroundColor = "#66ff33";
+}
+function cambiar_color_out(celda) {
+  celda.style.backgroundColor = "#dddddd";
 }
