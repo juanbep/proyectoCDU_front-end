27dfcdd04044e9f3dc1aacb4669b9e5c7635350b
Santiago Cordoba <65146938+santiago9739@users.noreply.github.com>
2022-07-28 14:49:06 -0500

    primera version del Agregar horario
    TODO: json

--- src/app/components/calendario/form-agregar-horario/form-agregar-horario.component.ts
+++ src/app/components/calendario/form-agregar-horario/form-agregar-horario.component.ts
@@ -1,177 +1,212 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { HorarioPK } from 'src/app/interfaces/horario-pk';
+import { Horario } from 'src/app/interfaces/horario';
 import { HorarioService } from 'src/app/services/horario.service';
 
 @Component({
   selector: 'app-form-agregar-horario',
   templateUrl: './form-agregar-horario.component.html',
-  styleUrls: ['./form-agregar-horario.component.css']
+  styleUrls: ['./form-agregar-horario.component.css'],
 })
-
 export class FormAgregarHorarioComponent implements OnInit {
+  horarioPk: HorarioPK = new HorarioPK();
+  listaHorarioPk: any[] = [];
+  horario: Horario = new Horario();
 
-  horario: HorarioPK = new HorarioPK();
-  horarioAux: HorarioPK = new HorarioPK();
+  opcionSeleccionadoInicio: number = 0;
+  verSeleccionInicio: number = 0;
 
-  aux: string = '';
-  aux4: string = '';
+  opcionSeleccionadoFin: number = 0;
+  verSeleccionFin: number = 0;
 
-  opcionSeleccionadoInicio:number=0;
-  verSeleccionInicio:number= 0;
+  opcionSeleccionadoEstudId: number = 0;
+  verEstudianteId: number = 0;
 
-  opcionSeleccionadoFin:number=0;
-  verSeleccionFin:number= 0;
+  opcionSeleccionadoProgId: number = 0;
+  verProgramaId: number = 0;
 
-  opcionSeleccionadoEstudId:number=0;
-  verEstudianteId:number= 0;
+  opcionSeleccionadoInicioDate = new Date();
+  varFechaInicio: Date = new Date();
 
-  opcionSeleccionadoProgId:number=0;
-  verProgramaId:number= 0;
-
-  opcionSeleccionadoInicioDate=new Date;
-  varFechaInicio:Date=new Date;
-
-  opcionSeleccionadoFinDate=new Date;
-  varFechaFin:Date=new Date;
+  opcionSeleccionadoFinDate = new Date();
+  varFechaFin: Date = new Date();
 
   listHorasInicio: any[] = [];
   listHorasFin: any[] = [];
 
-  listDias: any[] =[
-    { nombre: "Lunes", checked: false },
-    { nombre: "Martes", checked: false },
-    { nombre: "Miercoles", checked: false },
-    { nombre: "Jueves", checked: false },
-    { nombre: "Viernes", checked: false },
-    { nombre: "Sabado", checked: false },
-    { nombre: "Domingo", checked: false }
+  listDias: any[] = [
+    { nombre: 'Lunes', checked: false },
+    { nombre: 'Martes', checked: false },
+    { nombre: 'Miercoles', checked: false },
+    { nombre: 'Jueves', checked: false },
+    { nombre: 'Viernes', checked: false },
+    { nombre: 'Sabado', checked: false },
+    { nombre: 'Domingo', checked: false },
   ];
 
-  listDiasAux: any[] =[];
+  listDiasAux: any[] = [];
 
-  constructor(
-    private HorarioService : HorarioService,
-    private route: Router
-  ) { }
+  constructor(private HorarioService: HorarioService, private route: Router) {}
 
   ngOnInit(): void {
-    this.mostrarHorarioInicio()
+    this.mostrarHorarioInicio();
+    this.createHorario();
   }
 
-  onSubmit() {
-    this.mostrardias(); 
-  }
+  createHorarioPk(): void {
+    console.log('Entrando a crear horario pk');
+    this.horarioPk.horarioHoraInicio = this.verSeleccionInicio;
+    this.horarioPk.horarioHoraFin = this.verSeleccionFin;
+    this.horarioPk.horarioFechaInicio = this.varFechaInicio;
+    this.horarioPk.horarioFechaFin = this.varFechaFin;
 
-  /**
-   *createHorario(): void {
-    this.horarioAux.horarioHoraInicio = this.horarioForm.value.horarioHoraInicio;
-    this.horarioAux.horarioHoraFin = this.horarioForm.value.horarioHoraFin;
-    this.horarioAux.horarioDia = this.horarioForm.value.horarioDia;
-    this.horarioAux.horarioFechaInicio = this.horarioForm.value.horarioFechaInicio;
-    this.horarioAux.horarioFechaFin = this.horarioForm.value.horarioFechaFin;
-
-    this.HorarioService.create(this.horarioAux).subscribe((res) => this.route.navigate(['/calendario']));
+    for (let index = 0; index < this.listDiasAux.length; index++) {
+      this.horarioPk.horarioDia = this.listDiasAux[index];
+      console.log('dias ', this.listDiasAux[index]);
+      this.listaHorarioPk[index] = this.horarioPk;
     }
-   */
+    this.imprimirHoraPk();
+  }
 
-  mostrarHorarioInicio():void{
+  createHorario() {
+    console.log('Entrando a crear horario');
+
+    this.obtenerDias();
+    this.createHorarioPk();
+    this.horario.horarioEstado = 'fijo';
+    this.horario.programaId = this.verProgramaId;
+    this.horario.usuarioId = this.verEstudianteId;
+
+    for (let index = 0; index < this.listaHorarioPk.length; index++) {
+      this.horario.pk = this.listaHorarioPk[index];
+      this.HorarioService.create(this.horario).subscribe((res) =>
+        this.route.navigate(['/calendario'])
+      );
+      this.imprimirHora()
+    }
+  }
+
+  mostrarHorarioInicio(): void {
+    console.log('Entrando a mostrar hora inicio');
+
     for (let index = 0; index < 17; index++) {
-      this.listHorasInicio[index] = index+6;
+      this.listHorasInicio[index] = index + 6;
     }
-    console.log('gonorrea', this.listHorasInicio.length);
   }
 
-  mostrarHorarioFin():void{
-    let varAux=0
-    this.listHorasFin = []
+  mostrarHorarioFin(): void {
+    console.log('Entrando a mostrar hora fin');
+
+    let varAux = 0;
+    this.listHorasFin = [];
 
     for (let index = 0; index < this.listHorasInicio.length; index++) {
-      if(this.listHorasInicio[index]>=this.verSeleccionInicio){
-        if(this.listHorasFin[varAux] == 22 ){
-          break
-        }else{
-          this.listHorasFin[varAux] = this.listHorasInicio[index]
-          varAux = varAux + 1
+      if (this.listHorasInicio[index] >= this.verSeleccionInicio) {
+        if (this.listHorasFin[varAux] == 22) {
+          break;
+        } else {
+          this.listHorasFin[varAux] = this.listHorasInicio[index];
+          varAux = varAux + 1;
         }
       }
     }
   }
 
-  mostrardias():void{
-    for (let index = 0; index < 17; index++) {
-      this.aux4= this.listDias[index];
-      console.log('dias prueba', this.aux4);
+  mostrardias(): void {
+    console.log('Entrando a mostrar dias', this.listDiasAux.length);
+    for (let index = 0; index < this.listDiasAux.length; index++) {
+      console.log('dias prueba', this.listDiasAux[index]);
     }
   }
 
-  obtenerDias():void{
+  obtenerDias(): void {
+    console.log('Entrando a obtener dias');
     let index = 0;
+    this.listDiasAux = [];
 
-    if(this.listDias[0] == true){
-      this.listDiasAux[index] = "lunes"
-      index = index++;
+    if (this.listDias[0].checked == true) {
+      this.listDiasAux[index] = 'LUNES';
+      index = index + 1;
     }
 
-    if(this.listDias[1] == true){
-      this.listDiasAux[index] = "martes"
-      index = index++;
+    if (this.listDias[1].checked == true) {
+      this.listDiasAux[index] = 'MARTES';
+      index = index + 1;
     }
 
-    if(this.listDias[2] == true){
-      this.listDiasAux[index] = "miercoles"
-      index = index++;
+    if (this.listDias[2].checked == true) {
+      this.listDiasAux[index] = 'MIERCOLES';
+      index = index + 1;
     }
 
-    if(this.listDias[3] == true){
-      this.listDiasAux[index] = "jueves"
-      index = index++;
+    if (this.listDias[3].checked == true) {
+      this.listDiasAux[index] = 'JUEVES';
+      index = index + 1;
     }
 
-    if(this.listDias[4] == true){
-      this.listDiasAux[index] = "viernes"
-      index = index++;
+    if (this.listDias[4].checked == true) {
+      this.listDiasAux[index] = 'VIERNES';
+      index = index + 1;
     }
 
-    if(this.listDias[5] == true){
-      this.listDiasAux[index] = "sabado"
-      index = index++;
+    if (this.listDias[5].checked == true) {
+      this.listDiasAux[index] = 'SABADO';
+      index = index + 1;
     }
 
-    if(this.listDias[6] == true){
-      this.listDiasAux[index] = "domingo"
-      index = index++;
+    if (this.listDias[6].checked == true) {
+      this.listDiasAux[index] = 'DOMINGO';
+      index = index + 1;
     }
+    this.mostrardias();
   }
 
   capturarProgramaId() {
+    console.log('Entrando a obtener programa id');
     this.verProgramaId = this.opcionSeleccionadoProgId;
-    console.log('hora prueba inicio', this.verProgramaId);
+    console.log('programa id: ', this.verProgramaId);
   }
 
   capturarEstudianteId() {
+    console.log('Entrando a obtener estudiante id');
     this.verEstudianteId = this.opcionSeleccionadoEstudId;
-    console.log('hora prueba inicio', this.verEstudianteId);
+    console.log('estudiante id: ', this.verEstudianteId);
   }
 
   capturarHoraInicio() {
+    console.log('Entrando a capturar hora inicio');
     this.verSeleccionInicio = this.opcionSeleccionadoInicio;
-    console.log('hora prueba inicio', this.verSeleccionInicio);
+    console.log('hora inicio: ', this.verSeleccionInicio);
   }
 
   capturarHoraFin() {
+    console.log('Entrando a capturar hora fin');
     this.verSeleccionFin = this.opcionSeleccionadoFin;
-    console.log('hora prueba inicio', this.verSeleccionFin);
+    console.log('hora fin', this.verSeleccionFin);
   }
 
-  capturarFechasInicio(){
+  capturarFechasInicio() {
+    console.log('Entrando a capturar fecha inicio');
     this.varFechaInicio = this.opcionSeleccionadoInicioDate;
-    console.log('hora prueba inicio', this.varFechaInicio);
+    console.log('fecha inicio: ', this.varFechaInicio);
   }
 
-  capturarFechasFin(){
+  capturarFechasFin() {
+    console.log('Entrando a capturar fecha fin');
     this.varFechaFin = this.opcionSeleccionadoFinDate;
-    console.log('hora prueba inicio', this.varFechaFin);
+    console.log('fecha fin: ', this.varFechaFin);
+  }
+
+  imprimirHoraPk() {
+    console.log('Entrando a imprimir objeto hora pk');
+    for (let index = 0; index < this.listaHorarioPk.length; index++) {
+      console.log('Objeto hora pk: ', this.listaHorarioPk[index]);
+    }
+  }
+
+  imprimirHora() {
+    console.log('Entrando a imprimir objeto hora');
+    console.log('Objeto hora pk: ', this.horario);
   }
 }
--- src/app/interfaces/horario.ts
+++ src/app/interfaces/horario.ts
@@ -2,7 +2,7 @@ import { HorarioPK } from "./horario-pk";
 
 export class Horario {
     horarioEstado: string = "";
-    programaId: string = "";
-    usuarioId: string = "";
+    programaId: number = 0;
+    usuarioId: number = 0;
     pk: HorarioPK =  new HorarioPK();
 }
